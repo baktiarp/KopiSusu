90a0f0198b2d6eb91a54036d4194b11537849d7d
USER <USER@DESKTOP-JCSL8H9>
2018-09-05 13:52:38 +0700

    Commit Keseluruhan

---
+++ src/latihan3/Gajah.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Gajah extends Hewan {
+    public static void testclassMethod()
+    {
+        System.out.println("The class method in hewan...");
+    }
+    //Meng Override Method Pada Class Animal
+    public void testInstanceMethod()
+    {
+        System.out.println("The Instance Method In Gajah...");
+    }
+    
+    
+    public static void main(String[]Args)
+    {
+        Gajah myGajah = new Gajah();
+        Hewan myHewan = myGajah;
+        Hewan.testclassMethod();
+        myHewan.testInstanceMethod();
+    }
+}
